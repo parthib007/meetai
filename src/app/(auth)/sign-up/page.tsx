e2649793b4bd193c4b5e7169253bc3f063f4974e
Parthib Das <parthibdas007@gmail.com>
2025-07-30 19:41:07 +0530

    04: authentication UI

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+const Page = () => {
+    console.log("Sign Up Page Loaded");
+    return (
+            <SignUpView />
+    );
+    }
+export default Page;
